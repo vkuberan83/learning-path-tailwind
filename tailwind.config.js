780a46722f30c2e78eba4a31ef54a4acf4bee80e
Velmurugan Kuberan <vkuberan.dev@gmail.com>
2024-09-09 20:51:23 +0530

    chore: worked on various breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
     debugScreens: {
       position: ['top', 'left'],
     },
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      // 'md': { 'min': '768px', 'max': '1023px' },
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
     extend: {},
   },
   plugins: [
